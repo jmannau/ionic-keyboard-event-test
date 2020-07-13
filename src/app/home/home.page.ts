03c236a6b29f8672b52df622c3837562537bfe97
James Manners <james@manners.net.au>
2020-07-13 13:40:01 +1000

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Platform } from "@ionic/angular";
+import { merge } from "rxjs";
+import { mapTo, startWith, tap } from "rxjs/operators";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  keyboardIsVisible$ = merge(
+    this.platform.keyboardDidShow.pipe(
+      tap((keyboardOpenEvent) => console.log({ keyboardOpenEvent })),
+      mapTo(true)
+    ),
+    this.platform.keyboardDidHide.pipe(
+      tap((keyboardHideEvent) =>
+        console.log({ keyboardOpenEvent: keyboardHideEvent })
+      ),
+      mapTo(false)
+    )
+  ).pipe(startWith(false));
 
-  constructor() {}
-
+  constructor(private platform: Platform) {}
 }
